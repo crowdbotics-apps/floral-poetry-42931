4223d47d29f54a0e9740fe610a6a1db90df06aa9
Crowdbotics <team@crowdbotics.com>
2023-08-18 12:36:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-poetry-42931",
+  url: "https://floral-poetry-42931.botics.co",
+  api: "https://floral-poetry-42931.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
